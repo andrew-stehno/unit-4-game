0ed5e2748ddc682ccecf130a2e9d52aad4d8482c
andrew stehno <andrew_stehno@yahoo.com>
2019-09-08 20:07:46 -0600

    completed click functions to update game score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,80 +1,92 @@
 // .ready function should not be required as my script is at the bottom of the body.
 // ==============================================================
-$(document).ready(function() {
+$(document).ready(function () {
 
-// Game Data goes here:
-// ==============================================================
+    // Game Data goes here:
+    // ==============================================================
 
-let gameData = {
-    gems: ['emerald', 'jewel', 'ruby', 'topaz'],   
-    emeraldValue: gemValue(1, 12),
-    jewelValue: gemValue(1, 12),
-    rubyValue: gemValue(1, 12),
-    topazValue: gemValue(1, 12),
-    randomTarget: targetScore(19, 120),
-    scoreCounter: 0,
-    wins: 0,
-    losses: 0,
-}
+    let gameData = {
+        emeraldValue: gemValue(1, 12),
+        jewelValue: gemValue(1, 12),
+        rubyValue: gemValue(1, 12),
+        topazValue: gemValue(1, 12),
+        randomTarget: targetScore(19, 120),
+        scoreCounter: null,
+        wins: 0,
+        losses: 0,
+    }
     console.log("emerald value: " + gameData.emeraldValue);
     console.log("jewel value: " + gameData.jewelValue);
     console.log("ruby value: " + gameData.rubyValue);
     console.log("topaz value: " + gameData.topazValue);
     console.log("target value: " + gameData.randomTarget);
 
-// Core function invocation occurs here:
-// ==============================================================
+    // Core function invocation occurs here:
+    // ==============================================================
 
-function init() {
-    whenClicked();
-    targetScore();
-    gemValue();
-    displayAll();
-}
+    function init() {
+        whenClicked();
+        targetScore();
+        gemValue();
+        displayAll();
+    }
 
-// Game functions are listed below:
-// ==============================================================
+    // Game functions are listed below:
+    // ==============================================================
 
-function whenClicked() {
-$("#image-1").on("click", function() {
-        $("#score-is").text(gameData.scoreCounter + gameData.emeraldValue);
-    });
+    function whenClicked() {
+        $("#image-1").on("click", function () {
+            gameData.scoreCounter = gameData.scoreCounter + gameData.emeraldValue;
+            $("#score-is").text(gameData.scoreCounter);
+        });
 
-    $("#image-2").on("click", function() {
-        let jewel = gameData.scoreCounter + gameData.jewelValue;
-    });
+        $("#image-2").on("click", function () {
+            gameData.scoreCounter = gameData.scoreCounter + gameData.jewelValue;
+            $("#score-is").text(gameData.scoreCounter);
+        });
+
+        $("#image-3").on("click", function () {
+            gameData.scoreCounter = gameData.scoreCounter + gameData.rubyValue;
+            $("#score-is").text(gameData.scoreCounter);
+        });
+
+        $("#image-4").on("click", function () {
+            gameData.scoreCounter = gameData.scoreCounter + gameData.topazValue;
+            $("#score-is").text(gameData.scoreCounter);
+        });
 
 
-}
 
-function targetScore(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; 
-}       
+    }
 
-function gemValue(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}   
+    function targetScore(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
-function displayAll() {
-    $("#random-number").html(gameData.randomTarget);
-    $("#wins").html(gameData.wins);
-    $("#losses").html(gameData.losses);
-    $("#score-is").html(gameData.scoreCounter);
-}
+    function gemValue(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
-function resetGame() {
-    targetScore();
-    gemValue();
-}
+    function displayAll() {
+        $("#random-number").text(gameData.randomTarget);
+        $("#wins").html(gameData.wins);
+        $("#losses").html(gameData.losses);
+        $("#score-is").html(gameData.scoreCounter);
+    }
 
-init();
+    function resetGame() {
+        targetScore();
+        gemValue();
+    }
 
-// Main processes are listed below
-// ==============================================================
+    init();
+
+    // Main processes are listed below
+    // ==============================================================
 
 
 
